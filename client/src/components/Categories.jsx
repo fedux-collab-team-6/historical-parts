1d4c4cf5f26de79c036a0d985058c8bc59d6d9c8
Hassen Ahmed <hassan.creative@gmail.com>
2024-04-16 01:42:34 +0200

    finishing categories and products list pages

---
+++ client/src/components/Categories.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Categories = () => {
+  return (
+    <>
+      <h2 className=" text-center mb-7 text-3xl font-bold">Categories</h2>
+      <div className="categories flex h-[80vh] gap-2.5 m-2.5">
+        <div className="col">
+          <div className="row">
+            <img src="../public/images/cat-door.jpg" alt="" />
+            <button>
+              <Link className="link" to="/products/1">
+                Doors
+              </Link>
+            </button>
+          </div>
+          <div className="row">
+            <img src="../public/images/cat-window.jpg" alt="" />
+            <button>
+              <Link to="/products/1" className="link">
+                Windows
+              </Link>
+            </button>
+          </div>
+        </div>
+        <div className="col">
+          <div className="row">
+            {" "}
+            <img src="../public/images/cat-tile.jpg" alt="" />
+            <button>
+              <Link to="/products/1" className="link">
+                Tiles
+              </Link>
+            </button>
+          </div>
+        </div>
+        <div className="col col-l">
+          <div className="row">
+            <div className="col">
+              <div className="row">
+                <img src="../public/images/cat-roof.jpg" alt="" />
+                <button>
+                  <Link to="/products/1" className="link">
+                    Roofs
+                  </Link>
+                </button>
+              </div>
+            </div>
+            <div className="col">
+              <div className="row">
+                {" "}
+                <img src="../public/images/cat-floor.jpg" alt="" />
+                <button>
+                  <Link to="/products/1" className="link">
+                    Floors
+                  </Link>
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <img src="../public/images/cat-door.jpg" alt="" />
+            <button>
+              <Link to="/products/1" className="link">
+                Shoes
+              </Link>
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Categories;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Hero from "../../components/Hero";
 import FeaturedProducts from "../../components/FeaturedProducts";
+import Categories from "../../components/Categories";
 
 const Home = () => {
   return (
     <div>
       <Hero />
       <FeaturedProducts />
+      <Categories />
     </div>
   );
 };
