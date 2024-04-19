96c4db604faed604da95e129125d177d6cb442f0
Hassen Ahmed <hassan.creative@gmail.com>
2024-04-19 02:31:59 +0200

    centering the categories in the page and reordering

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -3,51 +3,50 @@ import { Link } from "react-router-dom";
 
 const Categories = () => {
   return (
-    <>
+    <div className=" flex flex-col justify-center items-center mt-10">
       <h2 className=" text-center mb-7 text-3xl font-bold">Categories</h2>
 
       <div className="categories">
         <button>
-          <img src="../public/images/cat-door.jpg" alt="Cat door" />
+          <img src="./images/cat-door.jpg" alt="Cat door" />
           <Link className="link" to="/products/1">
             Doors
           </Link>
         </button>
         <button>
-          <img src="../public/images/cat-window.jpg" alt="" />
+          <img src="./images/cat-window.jpg" alt="" />
           <Link to="/products/1" className="link">
             Windows
           </Link>
         </button>
         <button>
-          <img src="../public/images/cat-tile.jpg" alt="" />
+          <img src="./images/cat-tile.jpg" alt="" />
           <Link to="/products/1" className="link">
             Tiles
           </Link>
         </button>
         <button>
-          <img src="../public/images/cat-roof.jpg" alt="" />
+          <img src="./images/cat-roof.jpg" alt="" />
 
           <Link to="/products/1" className="link">
             Roofs
           </Link>
         </button>
         <button>
-          <img src="../public/images/cat-floor.jpg" alt="" />
+          <img src="./images/cat-floor.jpg" alt="" />
 
           <Link to="/products/1" className="link">
             Floors
           </Link>
         </button>
         <button>
-          <img src="../public/images/cat-door.jpg" alt="" />
+          <img src="./images/cat-door.jpg" alt="" />
           <Link to="/products/1" className="link">
             Shoes
           </Link>
         </button>
-
       </div>
-    </>
+    </div>
   );
 };
 
