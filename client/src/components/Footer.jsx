89d3ff9e9b19bdb0501e883ee1ab9a60508955bf
Hassen Ahmed <hassan.creative@gmail.com>
2024-04-24 17:40:25 +0200

    pushing footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,9 +3,9 @@ import { FaFacebookF, FaLinkedin, FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <footer className="footer">
-      <div className="footer_nav">
-        <ul>
+    <footer className="bg-spindle-950 text-light-200 flex justify-center items-center flex-col py-24 gap-6">
+      <div className="">
+        <ul className=" flex gap-12">
           <li>
             <a href="#">Home</a>
           </li>
@@ -27,7 +27,7 @@ const Footer = () => {
         </ul>
       </div>
       <div className="socials">
-        <ul>
+        <ul className=" flex gap-7">
           <li>
             <a href="#">
               <FaFacebookF size={20} />
@@ -49,55 +49,6 @@ const Footer = () => {
         <p>&copy; Copyright {new Date().getFullYear()} - Historical Parts</p>
       </div>
     </footer>
-    // <footer className="footer mt-24 mr-48 mb-5 ml-48">
-    //   {/* <div className="top flex gap-12">
-    //     <div className="item flex-1 flex flex-col gap-2.5 text-justify text-sm">
-    //       <h3 className=" text-lg font-medium text-gray-600">Categories</h3>
-    //       <span className=" text-sm text-gray-400">Doors</span>
-    //       <span className=" text-sm text-gray-400">Windows</span>
-    //       <span className=" text-sm text-gray-400">Tiles</span>
-    //       <span className=" text-sm text-gray-400">Roofs</span>
-    //     </div>
-    //     <div className="item flex-1 flex flex-col gap-2.5 text-justify text-sm">
-    //       <h3 className=" text-lg font-medium text-gray-600">Links</h3>
-    //       <span className=" text-sm text-gray-400">FAQ</span>
-    //       <span className=" text-sm text-gray-400">About Us</span>
-    //       <span className=" text-sm text-gray-400">Blog</span>
-    //       <span className=" text-sm text-gray-400">Contact Us</span>
-    //     </div>
-    //     <div className="item flex-1 flex flex-col gap-2.5 text-justify text-sm">
-    //       <h3 className=" text-lg font-medium text-gray-600">About</h3>
-    //       <span className=" text-sm text-gray-400">
-    //         Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
-    //         eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
-    //         amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
-    //         ut labore etdolore.
-    //       </span>
-    //     </div>
-    //     <div className="item flex-1 flex flex-col gap-2.5 text-justify text-sm">
-    //       <h3 className=" text-lg font-medium text-gray-600">Contact</h3>
-    //       <span className=" text-sm text-gray-400">
-    //         Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
-    //         eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
-    //         amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
-    //         ut labore etdolore.
-    //       </span>
-    //     </div>
-    //   </div>
-    //   <div className="bottom flex items-center justify-between mt-12">
-    //     <div className="left flex items-center">
-    //       <span className="logo text-[#2879fe] font-bold text-lg">
-    //         Historical Parts
-    //       </span>
-    //       <span className="copyright">
-    //         © Copyright 2024. All Rights Reserved
-    //       </span>
-    //     </div>
-    //     <div className="right ml-5 text-sm text-gray-400">
-    //       <img className=" h-12" src="/images/payment.png" alt="" />
-    //     </div>
-    //   </div> */}
-    // </footer>
   );
 };
 
