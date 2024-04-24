228cf35258e6771651d90e0b46b1c58fbb7c39bd
Hassen Ahmed <hassan.creative@gmail.com>
2024-04-24 18:28:34 +0200

    fixing category component

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -5,9 +5,7 @@ const Categories = () => {
   const { data, loading, error } = useFetch("/categories?populate=*");
   return (
     <div className=" flex flex-col justify-center items-center mt-10">
-      <h2 className=" text-center mb-7 text-3xl font-bold">Categories</h2>
-
-      <div className="flex flex-row justify-around w-scren overflow-x-scroll sm:overflow-x-hidden">
+      <div className="flex flex-row justify-center gap-10 w-full overflow-x-scroll sm:overflow-x-hidden">
         {loading ? (
           <p>Loading...</p>
         ) : error ? (
--- client/src/components/CategoryItem.jsx
+++ client/src/components/CategoryItem.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../components/style-components/categories.css";
+// import "../components/style-components/categories.css";
 
 const CategoryItem = ({ item }) => {
   console.log(item);
   return (
-    <Link className="link" to={`/products/${item.id}`}>
-      <img src={`${item.attributes?.img?.data?.attributes?.url}`} alt="" />
-      {item.attributes?.title}
+    <Link className=" overflow-hidden " to={`/products/${item.id}`}>
+      <div className=" flex flex-col justify-center items-center gap-3 border-coral-400 border rounded-full py-4 px-2 hover:bg-light-200 cursor-pointer">
+        <img src={`${item.attributes?.img?.data?.attributes?.url}`} alt="" />
+        <p className=" text-sm">{item.attributes?.title}</p>
+      </div>
     </Link>
   );
 };
