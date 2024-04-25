77d75aa017de04139a300d737841e71deb7fe6f7
Hassen Ahmed <hassan.creative@gmail.com>
2024-04-25 16:34:48 +0200

    working featured component

--- client/src/components/FeaturedProducts.jsx
+++ client/src/components/FeaturedProducts.jsx
@@ -1,23 +1,70 @@
 import React, { useState, useEffect } from "react";
 import ProductCard from "./ProductCard";
 import useFetch from "../../hooks/useFetch";
+import {
+  IoIosArrowDropleftCircle,
+  IoIosArrowDroprightCircle,
+} from "react-icons/io";
 
 const FeaturedProducts = () => {
   const { data, loading, error } = useFetch(
     "/products?filters[featured][$eq]=true&populate=*"
   );
 
+  console.log(data);
+  const [sliderId] = useState(
+    `slider-${Math.random().toString(36).substring(7)}`
+  );
+
+  const slideLeft = (sliderId) => {
+    let slider = document.getElementById(sliderId);
+    slider.scrollLeft = Math.max(slider.scrollLeft - 500);
+  };
+
+  const slideRight = (sliderId) => {
+    let slider = document.getElementById(sliderId);
+    slider.scrollLeft = Math.max(slider.scrollLeft + 500);
+  };
+
+  if (loading || !data) return <>Loading….</>;
+  if (error) return <>Something went wrong!</>;
+
   return (
-    <div className="featuredProducts my-24 mx-24">
-      <div className="top mb-12">
-        <h1 className=" text-3xl font-bold capitalize">Featured Products</h1>
+    <div className="featuredProducts py-14 bg-light-100 w-screen">
+      <div className="top mb-12 max-w-[1440px] mx-auto  w-full">
+        <h1 className=" text-3xl font-bold capitalize">Just Added</h1>
       </div>
-      <div className="bottom flex justify-center gap-12">
-        {error
-          ? "Something went wrong!"
-          : loading
-            ? "loading"
-            : data?.map((item) => <ProductCard item={item} key={item.id} />)}
+      <div
+        className="bottom bg-coral-400 flex justify-center w-screen items-center  group"
+        style={{ overflowX: "hidden" }}
+      >
+        <IoIosArrowDropleftCircle
+          onClick={() => {
+            slideLeft(sliderId);
+          }}
+          className=" absolute left-0 text-spindle-900 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
+        <div
+          id={sliderId}
+          className="overflow-x-scroll whitespace-nowrap scroll-smooth scrollbar-hide relative"
+        >
+          {data?.map((item) => (
+            <div
+              className="inline-block relative py-[1px] px-[.5px] hover:bg-m_darkGrey cursor-pointer"
+              key={item.id}
+            >
+              <ProductCard item={item} />
+            </div>
+          ))}
+        </div>
+        <IoIosArrowDroprightCircle
+          onClick={() => {
+            slideRight(sliderId);
+          }}
+          className=" absolute right-0 text-spindle-900 opacity-50 hover:opacity-100 cursor-pointer z-10 hidden group-hover:block"
+          size={40}
+        />
       </div>
     </div>
   );
--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -4,15 +4,15 @@ import { Link } from "react-router-dom";
 const ProductCard = ({ item }) => {
   return (
     <Link className="" to={`/product/${item.id}`}>
-      <div className="card w-72 flex flex-col gap-2.5 mb-12">
-        <div className="image w-full h-[400px] overflow-hidden relative">
-          <span className=" absolute top-1.5 left-1.5 bg-white text-teal-900 py-1 px-1.5 z-[3] font-semibold text-sm">
+      <div className="card w-72 bg-spindle-50 flex flex-col gap-2.5 p-8">
+        <div className="image w-full h-[250px] overflow-hidden relative">
+          {/* <span className=" absolute top-1.5 left-1.5 bg-white text-teal-900 py-1 px-1.5 z-[3] font-semibold text-sm">
             {item.attributes?.condition}
-          </span>
+          </span> */}
           <img
-            src={`${item.attributes?.img?.data[0]?.attributes?.url}`}
+            src={`${item.attributes?.vectorImg?.data?.attributes?.url}`}
             alt=""
-            className="mainImg w-full h-full object-cover"
+            className="mainImg w-full h-full object-contain"
           />
         </div>
         <div className=" flex justify-between items-center">
