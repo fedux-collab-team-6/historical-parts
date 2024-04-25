b7d9fcb18fe24b43ea8c5be378709da347a1eb7e
Hassen Ahmed <hassan.creative@gmail.com>
2024-04-25 03:32:50 +0200

    Fixing hero section

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,11 +1,5 @@
-//import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
-//import WestOutlinedIcon from "@mui/icons-material/WestOutlined";
+import { hero, logo } from "../constants/images";
 
-//const data = [
-// "../images/bnr1.jpg",
-// "../images/bnr2.webp",
-// "../images/bnr3.jpg",
-//];
 const Hero = () => {
   // const prevSlide = () => {
   //   setCurrentSlide(currentSlide === 0 ? 2 : (prev) => prev - 1);
@@ -15,8 +9,19 @@ const Hero = () => {
   // };
 
   return (
-    <div className="h-[calc(80vh-80px)] w-screen relative overflow-hidden">
-      <section className="hero">
+    <section className="h-[calc(80vh-80px)] w-screen relative overflow-hidden flex bg-spindle-200">
+      <div className="left flex-[3] p-9 flex justify-center items-center md:static absolute h-full w-full">
+        {/* <h1>HISTORICAL PARTS</h1> */}
+        <img src={logo} alt="header logo" />
+      </div>
+      <div className="right flex-1 ml-44 md:ml-0">
+        <img
+          className=" h-full w-full object-cover"
+          src={hero}
+          alt="hero image"
+        />
+      </div>
+      {/* <section className="hero">
         <div className="hero-content">
           <h1>HISTORICAL PARTS</h1>
         </div>
@@ -24,14 +29,14 @@ const Hero = () => {
           className="hero-image"
           style={{ backgroundImage: "url('./../images/hero.png')" }}
         ></div>
-      </section>
+      </section> */}
 
       {/* <div className="icons absolute w-full h-full flex items-center justify-center gap-2.5 z-10">
         <div className="icon w-[50px] h-[50px] border border-white flex items-center justify-center cursor-pointer text-white">
           <WestOutlinedIcon />
         </div>
       </div> */}
-    </div>
+    </section>
   );
 };
 
--- client/src/constants/images.js
+++ client/src/constants/images.js
@@ -2,5 +2,6 @@ import logo from "../assets/images/logo.svg";
 import logoWhite from "../assets/images/logoWhite.svg";
 import inspirationThumb from "../assets/images/inspirationThumb.png";
 import renovationThumb from "../assets/images/renovationThumb.png";
+import hero from "../assets/images/hero.png";
 
-export { logo, logoWhite, inspirationThumb, renovationThumb };
+export { logo, logoWhite, inspirationThumb, renovationThumb, hero };
